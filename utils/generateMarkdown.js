546b02a026b5feab67624925587c35fd85e6efda
fishmon <31542001+fishmon@users.noreply.github.com>
2024-02-03 08:29:37 +0000

    added generate md logic

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,47 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
 
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Features](#features)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+- [Screenshot](#screenshot)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Features
+${data.features}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contributing
+${data.contributors}
+
+## Tests
+To run tests, use the following command:
+\`\`\`
+${data.test}
+\`\`\`
+
+## Questions
+If you have any questions about the project, you can contact me via GitHub (${data.github}) or email (${data.email}).
+
+## Screenshot
+![Screenshot](/${data.screenshot})
 `;
 }
 
